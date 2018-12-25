0a804e4a22670168b56375d6b02b5af444b25d11
John Pham <johnphammail@gmail.com>
2018-12-24 20:10:32 -0800

    adding page stub

---
+++ src/pages/map.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Map" keywords={[`map`, `ucr`, `rose hack`]} />
+    <h1>Winston Chung</h1>
+    <h1>Bourns Hall Floor 2</h1>
+    <h1>Bourns Hall Floor 2</h1>
+    <h1>Bourns Hall Floor 2</h1>
+  </Layout>
+)
+
+export default IndexPage
