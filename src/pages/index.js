fd14bcb56f89d60c3d4da364f8cec0a376e6f798
John Pham <johnphammail@gmail.com>
2018-12-27 17:48:26 -0800

    adding prompt on homescreen for wifi request for mobile users #20

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,19 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Countdown from '../components/Countdown'
 import SocialFeed from '../components/SocialFeed'
-import { Header } from '../styled'
+import { Header, Wrapper } from '../styled'
+import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`event`, `live`, `portal`]} />
     <Header>Rose Hack 2019</Header>
+    <Wrapper className="showMobile">
+      <p>
+        Need access to Wi-Fi? Head over to the{' '}
+        <Link to="/wifi">request page</Link>.
+      </p>
+    </Wrapper>
     <Countdown />
     <SocialFeed />
   </Layout>
