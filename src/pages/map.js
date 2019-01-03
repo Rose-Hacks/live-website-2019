354242b2b615a6996eeb4686136df755e969f603
John Pham <johnphammail@gmail.com>
2019-01-03 15:07:53 -0800

    adding map

--- src/pages/map.js
+++ src/pages/map.js
@@ -3,14 +3,28 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Header, Wrapper } from '../styled'
 
-import Key from '../images/key.png'
+import Key from '../images/mapweb_key.svg'
+import FloorOne from '../images/mapweb_floor one.svg'
+import FloorTwo from '../images/mapweb_floor two.svg'
 
 const SchedulePage = () => (
   <Layout>
     <SEO title="Map" keywords={[`map`, 'events', 'workshops']} />
     <Header>Map</Header>
     <Wrapper>
-      <img src={Key} alt="" />
+      <img
+        src={Key}
+        alt="key for map"
+        style={{ display: `block`, margin: `0 auto`, maxWidth: `500px` }}
+      />
+    </Wrapper>
+
+    <Wrapper>
+      <img src={FloorOne} alt="map of floor 1" />
+    </Wrapper>
+
+    <Wrapper>
+      <img src={FloorTwo} alt="map of floor 2" />
     </Wrapper>
   </Layout>
 )
