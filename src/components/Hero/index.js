0f3d775247f77836e4526a8692353173918aa77b
John Pham <johnphammail@gmail.com>
2019-01-07 16:59:18 -0800

    adding hero animation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import Lottie from 'react-lottie'
+import animationData from './animation.json'
 
 const HeroContainer = styled.section`
   background: linear-gradient(100deg, #574565, #27346a);
   background-repeat: no-repeat;
-  height: 70vh;
   width: 100%;
+  div {
+    display: flex;
+
+    svg {
+      transform: translate3d(0px, 6px, 0px) !important;
+    }
+  }
 `
 
 const Hero = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  }
+
   return (
     <HeroContainer>
-      <h1>Hero</h1>
+      <Lottie options={defaultOptions} />
     </HeroContainer>
   )
 }
