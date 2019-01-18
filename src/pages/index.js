8dc11fcbff61915eb7efcdc42729f6901d99a939
John Pham <johnphammail@gmail.com>
2019-01-18 14:40:51 -0800

    adding animated title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,27 @@ import SocialFeed from '../components/SocialFeed'
 import { Header, Wrapper } from '../styled'
 import { Link } from 'gatsby'
 
+function loop() {
+  var i,
+    n,
+    s = ''
+
+  const title = ['R', 'O', 'S', 'E', ' ', 'H', 'A', 'C', 'K']
+
+  for (i = 0; i < 10; i++) {
+    n = Math.floor(Math.sin(Date.now() / 200 + i / 2) * 4) + 4
+
+    s += String.fromCharCode(0x2581 + n)
+    s += title[i]
+  }
+
+  document.title = s
+
+  setTimeout(loop, 50)
+}
+
+loop()
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`event`, `live`, `portal`]} />
