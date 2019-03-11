bdb9cf0d9e7213b39f03b35319956c666fe7e487
John Pham <johnphammail@gmail.com>
2019-03-10 19:53:18 -0700

    adding old website

--- src/globals/color.js
+++
@@ -1,8 +0,0 @@
-export const accent1 = '#da1e3b'
-export const accent2 = '#368d42'
-export const accent3 = '#46539c'
-export const accent4 = '#e8bf49'
-
-export const gradientStart = '#120843'
-export const gradientEnd = '#82476A'
-export const grandientAngle = 100
--- src/styled/index.js
+++
@@ -1,5 +0,0 @@
-import Wrapper from './Wrapper'
-import { Header, SubHeader, Channel, Emphasis } from './Typography'
-import Prompt from './Prompt'
-
-export { Wrapper, Header, SubHeader, Prompt, Channel, Emphasis }
